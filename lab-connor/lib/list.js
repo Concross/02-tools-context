cbf277b990154eaed0616f2fc0c5f11afea1cd16
Concross <cacrossley22@gmail.com>
2018-09-11 09:17:57 -0700

    remove extra whitespace

--- lab-connor/lib/list.js
+++ lab-connor/lib/list.js
@@ -3,21 +3,19 @@
 class List {
   constructor() {
     this.length = 0;
-
   }
 
   push(item) {
     this[this.length] = item;
     this.length++;
     return this.length;
-
   }
 
   pop() {
     if (!this.length) {
       return undefined;
-
     }
+
     let poppedItem = this[this.length - 1];
     delete this[this.length - 1];
     this.length--;
@@ -30,26 +28,23 @@ class List {
 
     if (end > this.length) {
       end = this.length;
-
     }
+
     for (let i = begin; i < end; i++) {
       sliceList.push(this[i]);
-
     }
-    sliceList.length = end - begin;
 
+    sliceList.length = end - begin;
     return sliceList;
   }
 
   forEach(callback) {
     for (let i = 0; i < this.length; i++) {
       callback(this[i], i, this);
-
     }
   }
 
   map(callback) {
-
     let mapList = new List();
     this.forEach((item, i) => mapList.push(callback(item, i)));
 
@@ -57,19 +52,16 @@ class List {
   }
 
   reduce(callback, initialValue) {
-
     if (!this.length && !initialValue) {
       throw new TypeError('TypeError');
 
     } else if (this.length === 1 && !initialValue) {
       return this[0];
-
     }
 
     let accumulator = initialValue || 0;
     for (let i = 0; i < this.length; i++) {
       accumulator = callback(accumulator, this[i], i, this);
-
     }
 
     return accumulator;
@@ -81,7 +73,6 @@ class List {
     this.forEach((item, i) => {
       if (callback(item, i)) {
         filterList.push(item);
-
       }
     });
 
